42404bd55e5fdfd0a2bdeac77d0b3161334f3ce6
Maxim Ciuchitu <maxim.c@goparrot.ai>
2021-09-16 10:54:35 +0300

    refactor(event): replace payload method with readonly field
    BREAKING CHANGE: Event paylod is no longer a method, but a field. It should not be invoked to
    retrieve its content

--- src/service/Publisher.ts
+++ src/service/Publisher.ts
@@ -13,7 +13,7 @@ export class Publisher<EventBase extends IEvent> extends DefaultPubSub<EventBase
     async publish<T extends EventBase>(event: T): Promise<void> {
         if (event instanceof PubsubEvent) {
             event.localEventEnabled() && super.publish(event);
-            return this.producer.produce(toEventName(event.constructor.name), event.payload(), event.exchange(), event.getOptions());
+            return this.producer.produce(toEventName(event.constructor.name), event.payload, event.exchange(), event.getOptions());
         }
 
         return super.publish(event);
